fbfc09c570a6242229188ac5c0afdc9806939fe8
alwaysready4moore <msnelson9241@gmail.com>
2026-07-11 18:15:20 -0400

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { SiteFooter } from "@/components/SiteFooter";
 import "./globals.css";
 
@@ -126,6 +127,7 @@ export default function RootLayout({
       <body>
         {children}
         <SiteFooter />
+        <Analytics />
       </body>
     </html>
   );
